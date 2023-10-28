90fa3b455eef37d9e6cbd1f2828ee9c273dc76ba
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-10-28 20:58:39 -0300

    style: config design colors and typography

---
+++ src/styles/colors.ts
@@ -0,0 +1,19 @@
+export const colors = {
+	base: {
+		gray100: '#F2F2F2',
+		gray200: '#D9D9D9',
+		gray300: '#808080',
+		gray400: '#333333',
+		gray500: '#262626',
+		gray600: '#1A1A1A',
+		gray700: '#0D0D0D',
+	},
+	feedback: {
+		danger: '#E25858',
+	},
+	background: '#131016',
+	blue: '#4EA8DE',
+	blueDark: '#1D4ED8',
+	purple: '#8284FA',
+	purpleDark: '#5E60CE',
+}
---
+++ src/styles/typography.ts
@@ -0,0 +1,13 @@
+export const typography = {
+    fontFamily: 'Inter',
+    fontSize: {
+        small: 12,
+        medium: 14,
+        large: 16
+    },
+    fontWeight: {
+        regular: '400',
+        bold: '700'
+    } as any,
+    lineWeight: '140%'
+}
