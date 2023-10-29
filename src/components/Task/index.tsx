02abe3c7a81e5505cfab3bceff26c41ebf14fa4e
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-10-28 23:58:42 -0300

    feat: create task component

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -2,13 +2,13 @@ import { Image, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./task.styles";
 import { Checkbox } from "../Checkbox";
 
-export function Task() {
+export function Task({ task }: { task: { id: number, title: string, isChecked: boolean } }) {
     return (
         <View style={styles.container}>
-            <Checkbox isChecked={true} />
+            <Checkbox isChecked={task.isChecked} />
             
             <Text style={styles.text}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                {task.title}
             </Text>
 
             <TouchableOpacity  style={styles.icon}>
