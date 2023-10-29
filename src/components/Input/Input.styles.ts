aaca1787455838ee8ea34e8a840cf6dd87e91e45
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-10-28 21:17:54 -0300

    style: config input component styles

---
+++ src/components/Input/Input.styles.ts
@@ -0,0 +1,21 @@
+import { colors, typography } from "../../styles";
+
+export const styles = {
+  input: {
+    backgroundColor: colors.base.gray500,
+    fontFamily: typography.fontFamily,
+    color: colors.base.gray100,
+    width: '100%' as any,
+    height: 54,
+    borderRadius: 8,
+    padding: 16,
+    top: -50
+  },
+  filledInput: {
+    borderColor: colors.purpleDark,
+    borderWidth: 1,
+  },
+  placeholder: {
+    color: colors.base.gray300,
+  },
+}
