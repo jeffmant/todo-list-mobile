1d9ecc4ce1477e624a1f4c4ee88b292ceb3f49cb
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-10-29 00:21:15 -0300

    feat: reactive add task

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,71 +1,28 @@
-import { FlatList, Image, View } from "react-native";
+import { View } from "react-native";
 import { styles } from "./home.styles";
 import { Input } from "../../components/Input";
 import { AddButton } from "../../components/AddButton";
 import { Header } from "../../components/Header";
-import { Task } from "../../components/Task";
 import { TaskList } from "../../components/TaskList";
+import { useState } from "react";
+import { TaskType } from "../../types/task";
 
 export function Home() {
-    const tasks = [
-      {
-        id: 1,
-        title: "Task 1",
-        isChecked: false,
-      },
-      {
-        id: 2,
-        title: "Task 2",
-        isChecked: false,
-      },
-      {
-        id: 3,
-        title: "Task 3",
-        isChecked: false,
-      },
-      {
-        id: 4,
-        title: "Task 4",
-        isChecked: false,
-      },
-      {
-        id: 5,
-        title: "Task 5",
-        isChecked: false,
-      },
-      {
-        id: 6,
-        title: "Task 6",
-        isChecked: false,
-      },
-      {
-        id: 7,
-        title: "Task 7",
-        isChecked: false,
-      },
-      {
-        id: 8,
-        title: "Task 8",
-        isChecked: false,
-      },
-      {
-        id: 9,
-        title: "Task 9",
-        isChecked: false,
-      },
-      {
-        id: 10,
-        title: "Task 10",
-        isChecked: false,
-      }
-    ]
+    const [tasks, setTasks] = useState<TaskType[]>([])
+    const [newTask, setNewTask] = useState('')
+
+    function handleAddTask () {
+      setTasks([...tasks, { id: tasks.length + 1, title: newTask, isChecked: false }])
+      setNewTask('')
+    }
+
     return (
 			<>
 				<Header />
         <View style={styles.container}>
 						<View style={styles.form}>
-							<Input />
-							<AddButton />
+							<Input value={newTask} placeholder="Adicione uma nova tarefa" onChange={setNewTask} />
+							<AddButton onPress={handleAddTask} />
 						</View>
 						<View>
               <TaskList tasks={tasks} />
